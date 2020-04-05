8cf5f0af5e486f82c771d30242da9c11cfe2bb9e
MiguelMenendezOrenga <49918305+MiguelMenendezOrenga@users.noreply.github.com>
2020-04-05 06:58:31 +0200

    Update plotErrorSample.R

--- R/plotErrorSample.R
+++ R/plotErrorSample.R
@@ -1,8 +1,14 @@
-plotErrorSample <- function(x, point = TRUE, ...){
+plotErrorSample <- function(x, point = TRUE, printlegend = TRUE, ...){
   graphics::plot(x$Evect, x$nvect, type = "l", ...)
   if(point == TRUE){
     graphics::points(x$Error, x$n, type = "p")
     graphics::text(x$Error + x$Error / 2, x$n, paste("n = ", round(x$n, 1)))
-    graphics::legend("topright", legend = paste("alfa =", x$alfa), bty = "n")
+  }
+  if(printlegend == TRUE){
+    graphics::legend("topright",
+                     legend = c(paste("alfa =", x$alfa),
+                                paste("p/q =", x$p, "/", 1-x$p),
+                                paste("Error =", x$Error)),
+                     bty = "n")
   }
 }
