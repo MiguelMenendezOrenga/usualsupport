a4736f14a914d033c5bd80801240c0bee193b84f
MiguelMenendezOrenga <49918305+MiguelMenendezOrenga@users.noreply.github.com>
2020-04-05 07:59:41 +0200

    Update plotErrorSample.R

--- R/plotErrorSample.R
+++ R/plotErrorSample.R
@@ -5,10 +5,24 @@ plotErrorSample <- function(x, point = TRUE, printlegend = TRUE, ...){
     graphics::text(x$Error + x$Error / 2, x$n, paste("n = ", round(x$n, 1)))
   }
   if(printlegend == TRUE){
-    graphics::legend("topright",
-                     legend = c(paste("alfa =", x$alfa),
-                                paste("p/q =", x$p, "/", 1-x$p),
-                                paste("Error =", x$Error)),
-                     bty = "n")
+    if(any(names(x) %in% c("sigma", "p"))){
+      graphics::legend("topright",
+                       legend = c(paste("alfa =", x$alfa),
+                                  # this chunk checks wether "sigma" or "p/q" should be printed
+                                  if(any(names(x) == "p")){
+                                    paste("p/q =", x$p, "/", 1-x$p)
+                                  }else{
+                                    if(any(names(x) == "sigma")){
+                                      paste("sigma =", x$sigma)
+                                    }
+                                  }
+                       ),
+                       bty = "n")
+
+    }else{
+      warning(
+      "No sigma nor p/q values could be found. Please check the input used.
+  If it's OK and you don't want to get this warning, set printlegend = FALSE")
+    }
   }
 }
